9869130eae9e8232edfeebc2fa82ac47351a6619
Rosa Wu <u10050070@gmail.com>
2018-05-04 17:39:07 +0800

    update connection method

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import json
 import urllib2
 import sys
 
-def ranking(temp,r1,r2,r3,r4):
+def ranking(temp,r1,r2,r3,r4,r5,r6):
     if temp <= 25:
         num = int(round(temp/5,0))
         r1 = '='*num+ ' '*(5-num)
@@ -19,12 +19,20 @@ def ranking(temp,r1,r2,r3,r4):
         num = int(round((temp-75)/5,0))
         r1,r2,r3 = '='*5,'='*5,'='*5
         r4 = '='*num+ ' '*(5-num)
+    elif temp <= 125:
+        num = int(round((temp-100)/5,0))
+        r1,r2,r3,r4 = '='*5,'='*5,'='*5,'='*5
+        r5 = '='*num+ ' '*(5-num)
+    elif temp <= 150:
+        num = int(round((temp-125)/5,0))
+        r1,r2,r3,r4,r5 = '='*5,'='*5,'='*5,'='*5,'='*5
+        r6 = '='*num+ ' '*(5-num)
 
-    print("[%s|%s|%s|%s]" % (r1,r2,r3,r4))
-    print("0     25   50   75   100")
+    print("[%s|%s|%s|%s|%s|%s]" % (r1,r2,r3,r4,r5,r6))
+    print("0     25   50   75   100   125   150")
 
     
-rsp=urllib2.urlopen('http://opendata.epa.gov.tw/ws/Data/AQX/?format=csv&ndctype=CSV&ndcnid=6074')
+rsp=urllib2.urlopen('https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000259/?format=csv&token={TOKEN}')
 data=rsp.read()
 csvdata=str(data).decode('utf8')
 csv=[]
@@ -43,38 +51,39 @@ for line in csv:
     i+=1    
         
 dict1=sorted(dict1.values(),reverse=True)
-r1,r2,r3,r4=' '*5,' '*5,' '*5,' '*5
-#print json.dumps(dict1, encoding="UTF-8", ensure_ascii=False)
+r1,r2,r3,r4,r5,r6=' '*5,' '*5,' '*5,' '*5,' '*5,' '*5
+
 x=0
 while x==0:
     print("=====即時空汙品質查詢=====")
-    a=input("調查各地即時PSI濃度排序請按1\n調查區域空汙品質查詢請按2\n")
+    a=input("調查各地即時AQI濃度請按1\n調查區域空汙品質查詢請按2\n")
     if(int(a)==1):
         for w in range(1,i):
+            if dict1[w-1][0]=="":
+                break;
             temp=dict1[w-1][0]
             temp=int(temp)
             print json.dumps(dict1[w-1][0:4], encoding="UTF-8", ensure_ascii=False).strip('"')
-            ranking(temp,r1,r2,r3,r4)
+            ranking(temp,r1,r2,r3,r4,r5,r6)
             print("\n")
-
+            
     if(int(a)==2):
         b=raw_input("輸入調查區域:").decode(sys.stdin.encoding)
         for w in range(1,i):
             c=dict1[w-1][1]
             if b==c:
-                print (' PSI｜'),json.dumps(dict1[w-1][0], encoding="UTF-8", ensure_ascii=False).strip('"')
+                print ('空氣品質指標AQI｜'),json.dumps(dict1[w-1][0], encoding="UTF-8", ensure_ascii=False).strip('"')
                 print ('區域｜'),json.dumps(dict1[w-1][1], encoding="UTF-8", ensure_ascii=False).strip('"')
                 print ('城市｜'),json.dumps(dict1[w-1][2], encoding="UTF-8", ensure_ascii=False).strip('"')
-                print ('狀態｜'),json.dumps(dict1[w-1][3], encoding="UTF-8", ensure_ascii=False).strip('"')
-                print ('氣體｜'),json.dumps(dict1[w-1][4], encoding="UTF-8", ensure_ascii=False).strip('"')
+                print ('氣體｜'),json.dumps(dict1[w-1][3], encoding="UTF-8", ensure_ascii=False).strip('"')
+                print ('狀態｜'),json.dumps(dict1[w-1][4], encoding="UTF-8", ensure_ascii=False).strip('"')
+                
     b=input("\n謝謝你的使用\n\n若要結束請按0\n若要繼續請按1\n")
     if(int(b)==0):
         print("\nGOODBYE!")
         x=1
     if(int(b)==1):
         x=0
-        print("\n")
-        
+        print("\n")        
 
     
-
